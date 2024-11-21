38b964998cdc86c25fe50d81ba27307e36b82271
dany80213 <163436703+dany80213@users.noreply.github.com>
2024-11-21 11:26:54 +0100

    Add files via upload

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("D:\\progetti video\\documentazione\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("D:\\progetti video\\documentazione\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("D:\\progetti video\\documentazione\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("D:\\progetti video\\documentazione\\my-website\\src\\css\\custom.css"),
+];
